10845a8feb676db1a2cb2783b07385fe68f2c022
srinivas1405-iiit <srinivasacharya1405@gmail.com>
2024-12-15 13:00:04 +0530

    readme updated

--- script.js
+++ script.js
@@ -26,12 +26,15 @@ function visualizeData(data) {
     // Define SVG dimensions
     const width = 800;
     const height = 600;
+    const margin = { top: 20, right: 30, bottom: 40, left: 40 };
 
     // Create SVG element
     const svg = d3.select("#map")
                   .append("svg")
-                  .attr("width", width)
-                  .attr("height", height);
+                  .attr("width", width + margin.left + margin.right)
+                  .attr("height", height + margin.top + margin.bottom)
+                  .append("g")
+                  .attr("transform", `translate(${margin.left},${margin.top})`);
 
     // Define scales for coordinates
     const xScale = d3.scaleLinear()
@@ -40,7 +43,7 @@ function visualizeData(data) {
 
     const yScale = d3.scaleLinear()
                      .domain([0, 1])
-                     .range([0, height]);
+                     .range([height, 0]); // Flip y-axis
 
     // Define icons for file types
     const icons = {
@@ -50,6 +53,15 @@ function visualizeData(data) {
         "mp3": "🎵",
         "pptx": "📊"
     };
+
+    // Add x-axis
+    svg.append("g")
+       .attr("transform", `translate(0, ${height})`)
+       .call(d3.axisBottom(xScale));
+
+    // Add y-axis
+    svg.append("g")
+       .call(d3.axisLeft(yScale));
 
     // Add tooltip element
     const tooltip = d3.select("#tooltip");
